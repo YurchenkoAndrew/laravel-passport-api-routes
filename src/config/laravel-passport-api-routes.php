832010dafd85dad857b2822909469ef7bc82a83a
YurchenkoAndrew <webmaster@wsa.kz>
2023-09-29 14:43:12 +0600

    Updated dependencies in the configuration.

--- src/config/laravel-passport-api-routes.php
+++ src/config/laravel-passport-api-routes.php
@@ -1,6 +1,17 @@
 <?php
+
+use YurchenkoAndrew\LaravelPassportAPIRoutes\Models\User;
+
 return [
     'app-url' => env('APP_URL', 'localhost'),
     'passport-client' => env('PASSPORT_CLIENT', 2),
     'passport-client-secret' => env('PASSPORT_CLIENT_SECRET'),
+
+    'providers' => [
+        'users' => [
+            'driver' => 'eloquent',
+            'model' => User::class,
+        ],
+    ],
+
 ];
