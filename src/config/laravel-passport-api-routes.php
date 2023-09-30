72255d4960bda7d8b57f237ddd191abb1241d514
YurchenkoAndrew <webmaster@wsa.kz>
2023-10-01 00:10:26 +0600

    Added constants to the configuration.

--- src/config/laravel-passport-api-routes.php
+++ src/config/laravel-passport-api-routes.php
@@ -3,7 +3,9 @@
 use YurchenkoAndrew\LaravelPassportAPIRoutes\Models\User;
 
 return [
-    'app-url' => env('APP_URL', 'localhost'),
+    'app-url' => env('APP_URL', 'http://localhost'),
+    'app-front-url' => env('APP_FRONT_URL', 'http://localhost:4200'),
+    'after-register-email-confirmation-route' => env('AFTER_REGISTER_EMAIL_CONFIRMATION_ROUTE', '/auth/login?verified=true'),
     'passport-client' => env('PASSPORT_CLIENT', 2),
     'passport-client-secret' => env('PASSPORT_CLIENT_SECRET'),
 
